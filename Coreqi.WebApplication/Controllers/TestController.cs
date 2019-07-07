ceac3ceaca5d2ef28825f1179a516d08bd6f2f43
fanqi <fanqisoft@gmail.com>
2019-07-07 09:07:55 +0800

    添加一些示例

--- Coreqi.WebApplication/Controllers/TestController.cs
+++ Coreqi.WebApplication/Controllers/TestController.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac.Annotation;
+using Coreqi.Services.Common.Interface;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Coreqi.WebApplication.Controllers
 {
+    [Bean]
     public class TestController : Controller
     {
+        [Autowired]
+        public IUserService userService { get; set; }
         public IActionResult Index()
         {
             return View();
